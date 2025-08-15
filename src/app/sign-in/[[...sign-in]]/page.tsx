37780d08fe9cb48795ba0bfb6e3e92b4feca318e
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2025-08-15 14:55:11 +0530

    centered

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,10 +2,12 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
+    <div className="min-h-screen w-full flex items-center justify-center p-4">
     <SignIn
       afterSignInUrl="/create"
       signInForceRedirectUrl="/create"
       signInFallbackRedirectUrl="/create"
     />
+    </div>
   )
 }
